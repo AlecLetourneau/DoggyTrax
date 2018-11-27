c33b20e5ff7271c77a9b38938885a9cc0b11c1df
Alec Letourneau <alecletourneau@gmail.com>
2018-11-27 14:56:55 -0600

    initial commit

---
+++ nRF5_SDK_15.0.0_a53641a/examples/ble_peripheral/ble_app_blinky/pca10040/s132/ses/Output/ble_app_blinky_pca10040_s132 Release/Obj/system_nrf52.d
@@ -0,0 +1,16 @@
+Output/ble_app_blinky_pca10040_s132\ Release/Obj/system_nrf52.o: \
+ C:\Users\MikeWaddick\Desktop\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\system_nrf52.c \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.40/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.40/include/stdbool.h \
+ C:\Users\MikeWaddick\Desktop\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\nrf.h \
+ C:\Users\MikeWaddick\Desktop\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ C:\Users\MikeWaddick\Desktop\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\system_nrf52.h \
+ C:\Users\MikeWaddick\Desktop\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\nrf52_bitfields.h \
+ C:\Users\MikeWaddick\Desktop\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\nrf51_to_nrf52.h \
+ C:\Users\MikeWaddick\Desktop\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\nrf52_name_change.h \
+ C:\Users\MikeWaddick\Desktop\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\compiler_abstraction.h
